a1ef253662e71d3d306d0b9641da1b6708302a2d
Rick te Brake <ricktebrake@gmail.com>
2019-05-24 15:28:09 +0200

    Added logging to resource

--- user-service/src/main/java/nl/quintor/userservice/resource/UserResource.java
+++ user-service/src/main/java/nl/quintor/userservice/resource/UserResource.java
@@ -2,6 +2,8 @@ package nl.quintor.userservice.resource;
 
 import nl.quintor.userservice.domain.User;
 import nl.quintor.userservice.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,16 +15,24 @@ import java.util.UUID;
 @RequestMapping("/users")
 public class UserResource {
 
+    Logger logger = LoggerFactory.getLogger(UserResource.class);
+
+
     @Autowired
     private UserRepository userRepository;
 
     @GetMapping
     public Iterable<User> getUsers() {
+        logger.info("All users requested");
+
         return userRepository.findAll();
     }
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<User> findUser(@PathVariable("id") UUID id) {
+
+        logger.info("User requested with id: " + id.toString());
+
         return userRepository.findById(id)
                 .map(ResponseEntity::ok)
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
@@ -30,6 +40,8 @@ public class UserResource {
 
     @PostMapping(consumes = "application/json")
     public User insertUser(@RequestBody User user) {
-        return userRepository.save(new User(UUID.randomUUID(), user.getUsername()));
+        User result = userRepository.save(new User(UUID.randomUUID(), user.getUsername()));
+        logger.info("New user created with id: " + user.getId().toString());
+        return result;
     }
 }
